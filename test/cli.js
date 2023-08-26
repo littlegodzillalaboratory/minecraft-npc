f8ec2a48d8c9d5228ebab27f29acdccf9065b632
Cliffano Subagio <cliffano@gmail.com>
2023-08-26 11:48:20 +1000

    Add unit test for cli-start.

--- test/cli.js
+++ test/cli.js
@@ -1,6 +1,6 @@
 "use strict"
 /* eslint no-unused-vars: 0 */
-import AE86 from '../lib/ae86.js';
+import MinecraftBob from '../lib/minecraftbob.js';
 import bag from 'bagofcli';
 import cli from '../lib/cli.js';
 import referee from '@sinonjs/referee';
@@ -16,6 +16,36 @@ describe('cli - exec', function() {
       done();
     };
     sinon.stub(bag, 'command').value(mockCommand);
+    cli.exec();
+  });
+});
+
+describe('cli - start', function() {
+
+  beforeEach(function () {
+    this.mockBag = sinon.mock(bag);
+  });
+
+  afterEach(function () {
+    this.mockBag.verify();
+    this.mockBag.restore();
+  });
+
+  it('should contain start command and delegate to minecraftbob start when exec is called', function (done) {
+    // this.mockBag.expects('logStepHeading').withExactArgs('Creating example AE86 project');
+    sinon.stub(bag, 'command').value(function (base, actions) {
+      actions.commands.start.action({
+        host: 'localhost',
+        port: 25565,
+        viewerPort: 3000,
+        username: 'someuser',
+        password: 's0m3p4ss'
+      });
+    });
+    sinon.stub(MinecraftBob.prototype, 'start').value(function (cb) {
+      assert.equals(typeof cb, 'function');
+      done();
+    });
     cli.exec();
   });
 });
