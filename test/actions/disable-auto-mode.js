22ca8d5f6377495d87481a703a66e700a05f61bb
Cliffano Subagio <cliffano@gmail.com>
2026-09-25 11:57:25 +1000

    Improve AutoMode comment. Add auto mode logging.

--- test/actions/disable-auto-mode.js
+++ test/actions/disable-auto-mode.js
@@ -1,23 +1,33 @@
 "use strict";
 import referee from "@sinonjs/referee";
 import sinon from "sinon";
+import bag from "bagofcli";
 
 import DisableAutoModeAction from "../../lib/actions/disable-auto-mode.js";
 
 const assert = referee.assert;
 
 describe("DisableAutoModeAction", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
   it("should disable auto mode and register the outcome", async () => {
+    const logStepHeading = sinon.stub(bag, "logStepHeading");
     const disableAutoMode = sinon.stub().resolves("success");
     const setActionInfo = sinon.spy();
     const action = new DisableAutoModeAction({
       disableAutoMode: disableAutoMode,
+      getAutoModeStatus: () => ({ engine: "survivor" }),
       getRegister: () => ({ setActionInfo: setActionInfo }),
     });
 
     await action.do({});
 
     assert.equals(disableAutoMode.callCount, 1);
+    assert.equals(logStepHeading.firstCall.args, [
+      "Disabling auto mode as survivor...",
+    ]);
     assert.equals(setActionInfo.firstCall.args[1], "success");
   });
 });
--- test/actions/enable-auto-mode.js
+++ test/actions/enable-auto-mode.js
@@ -1,13 +1,19 @@
 "use strict";
 import referee from "@sinonjs/referee";
 import sinon from "sinon";
+import bag from "bagofcli";
 
 import EnableAutoModeAction from "../../lib/actions/enable-auto-mode.js";
 
 const assert = referee.assert;
 
 describe("EnableAutoModeAction", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
   it("should enable auto mode and register the outcome", async () => {
+    const logStepHeading = sinon.stub(bag, "logStepHeading");
     const enableAutoMode = sinon.stub().returns("success");
     const setActionInfo = sinon.spy();
     const action = new EnableAutoModeAction({
@@ -19,6 +25,24 @@ describe("EnableAutoModeAction", () => {
 
     assert.equals(enableAutoMode.callCount, 1);
     assert.equals(enableAutoMode.firstCall.args, ["survivor"]);
+    assert.equals(logStepHeading.firstCall.args, [
+      "Enabling auto mode as survivor...",
+    ]);
     assert.equals(setActionInfo.firstCall.args[1], "success");
+  });
+
+  it("should log the configured engine when the command omits it", async () => {
+    const logStepHeading = sinon.stub(bag, "logStepHeading");
+    const action = new EnableAutoModeAction({
+      enableAutoMode: sinon.stub().returns("success"),
+      getAutoModeStatus: () => ({ engine: "survivor" }),
+      getRegister: () => ({ setActionInfo: sinon.spy() }),
+    });
+
+    await action.do({});
+
+    assert.equals(logStepHeading.firstCall.args, [
+      "Enabling auto mode as survivor...",
+    ]);
   });
 });
